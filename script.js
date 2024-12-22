d9ec61d96d4c16bb05dda5a3c0c692ffef1f4a31
Rostislav <105703069+Verilest@users.noreply.github.com>
2024-12-22 12:21:15 +0300

    v1.0

---
+++ script.js
@@ -0,0 +1,10 @@
+$('.carusel1').slick({
+  dots: false,
+  infinite: true,
+  prevArrow: '<img class="caruselarrow1" src="images/Arrow_left.svg">',
+  nextArrow: '<img class="caruselarrow2" src="images/Arrow_right.svg">',
+  speed: 600,
+  slidesToShow: 1,
+  adaptiveHeight: false,
+
+  });
